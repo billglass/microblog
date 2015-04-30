14200805a17b8c5c184366328957526bc8ebb01e
Bill Glass <williamtg087@gmail.com>
2015-04-30 14:16:35 -0400

    added bundler/setup to main.rb

--- main.rb
+++ main.rb
@@ -2,10 +2,10 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'rack-flash'
 require 'pry'
+require 'bundler/setup'
 
 set :database, "sqlite3:comic.sqlite3"
 set :sessions, true
-enable :sessions
 use Rack::Flash, sweep: true
 
 get '/' do
