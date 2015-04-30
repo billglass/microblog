0641ddfae1ba07b26ba0b29f475f3dcb99ec5fd9
Bradley <showalter.bradley@gmail.com>
2015-04-30 14:22:01 -0400

    Merge conflicts

--- main.rb
+++ main.rb
@@ -13,23 +13,11 @@ get '/' do
 end
 
 post '/login' do
-	@user = User.find_by(params[:user])
-		if @user and @user.password == params[:password]
-			session[:user_id] = @user.id
-			flash[:notice] = "Welcome Back #{@user}"
-			redirect to ('/feed')
-		else flash[:alert] = "Incorrect username/password"
-			redirect to ('/')
-		end
+
 end
 
 post '/sign_up' do
-	# Usernames are unique - loop through usernames, if @new_user is taken already, flash :alert?
-	@new_user = User.create(params[:user])
-	session[:user_id] = @new_user.id
-	flash[:notice] = "Welcome #{@new_user}"
-	puts "#{params.inspect}"
-	redirect to ('/account')
+
 end
 
 get '/account' do
