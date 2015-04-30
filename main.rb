b53a4ad3aac2cacb1f1f33f3bb69a5e09d2d51e9
Bradley <showalter.bradley@gmail.com>
2015-04-30 11:41:27 -0400

    Add basic pages to main.rb

--- main.rb
+++ main.rb
@@ -8,3 +8,34 @@ set :sessions, true
 enable :sessions
 use Rack::Flash, sweep: true
 
+get '/' do
+	erb :home
+end
+
+post '/' do
+	#something
+end
+
+get '/account' do
+	erb :account
+end
+
+post '/account' do
+	#something
+end
+
+get '/feed' do
+	erb :feed
+end
+
+post '/feed' do
+	#somthing
+end
+
+get '/profile' do
+	erb :profile
+end
+
+post '/profile' do
+	#something
+end
