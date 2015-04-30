506200c07e03c7ca3ef941f08a67358620807427
Bill Glass <williamtg087@gmail.com>
2015-04-30 14:29:43 -0400

    created table

--- db/migrate/20150430181824_create_users_table.rb
+++ db/migrate/20150430181824_create_users_table.rb
@@ -1,4 +1,17 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
+  	create_table :users do |t|
+  		t.string :username
+  		t.string :email
+  		t.string :password
+  		t.string :fname
+  		t.string :lname
+  		t.integer :phone
+  		t.string :city
+  		t.integer :birthday
+  		t.string :gender
+  		t.string :superhero
+  		t.string :website
+  	end
   end
 end
