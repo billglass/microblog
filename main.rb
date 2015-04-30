4de14ee86fd71d517c86c2bfdd82630c631f9e7d
Bradley <showalter.bradley@gmail.com>
2015-04-30 14:55:11 -0400

    Dumb changes

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'rack-flash'
+# require 'rack-flash'
 require 'pry'
 require 'bundler/setup'
 require './models'
 
 set :database, "sqlite3:comic.sqlite3"
 set :sessions, true
+# enable :sessions
 use Rack::Flash, sweep: true
 
 get '/' do
